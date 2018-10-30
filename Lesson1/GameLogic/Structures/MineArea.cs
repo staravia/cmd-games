cecfe145f939de6fae0153cd35f161083c45f6a2
Staravia <destaravia@gmail.com>
2018-10-29 17:59:58 -0700

    Update

---
+++ Lesson1/GameLogic/Structures/MineArea.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSAssignments.GameLogic.Structures
+{
+    /// <summary>
+    /// An area in a MineField used for gameplay.
+    /// </summary>
+    public enum MineArea
+    {
+        Unknown,
+        Revealed,
+        Safe,
+        Mine
+    }
+}
